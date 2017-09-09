69cb91e5274097cacb49bb38de2b4cd57ca45e47
Fábio Lourenço <faboyds@gmail.com>
2017-09-09 11:00:00 +0100

    [Back-end] Bootstrap class created and some minor fixes

---
+++ uniquiz/bootstrap/src/main/java/Import.java
@@ -0,0 +1,9 @@
+/**
+ * Created by fabiolourenco on 09/09/17.
+ */
+public class Import {
+    public static void imp(){
+
+    }
+
+}
--- uniquiz/src/main/java/controller/SolutionController.java
+++ uniquiz/src/main/java/controller/SolutionController.java
@@ -1,14 +1,11 @@
 package controller;
 
-import dto.QuizDTO;
 import dto.SolutionDTO;
-import model.Quiz;
 import model.Solution;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import repositories.QuizRepository;
 import repositories.SolutionRepository;
 
 import javax.persistence.NoResultException;
